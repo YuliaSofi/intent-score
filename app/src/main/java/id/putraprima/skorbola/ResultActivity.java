66c9441ccfa2f186759b94ad150907aa0305def2
YuliaSofi <yuliasofi92@gmail.com>
2020-03-06 22:37:07 +0700

    intent score

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -1,14 +1,39 @@
 package id.putraprima.skorbola;
 
+import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import id.putraprima.skorbola.R;
 
 public class ResultActivity extends AppCompatActivity {
+
+    TextView result;
+    TextView winner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        result = findViewById(R.id.result);
+        winner = findViewById(R.id.winner);
+
+        Bundle extras = getIntent().getExtras();
+        int homeResult = extras.getInt("homeScore");
+        int awayResult = extras.getInt("awayScore");
+        String homeName = extras.getString("homeName");
+        String awayName = extras.getString("awayName");
+
+        if(extras != null ){
+            result.setText("Score Akhir : "+String.valueOf(homeResult) + " - " + String.valueOf(awayResult));
+            if(homeResult > awayResult){
+                winner.setText("Tim "+homeName + " adalah pemenang!");
+            }else if(awayResult > homeResult){
+                winner.setText("Tim "+awayName + " adalah pemenang!");
+            }else{
+                winner.setText("Tim seimbang!");
+            }
+        }
     }
 }
